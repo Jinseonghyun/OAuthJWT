e2eb5304e46c05c8dd32aa9b6d7916ac8fa5ca5b
Jinseonghyun <jinsunghyun9@gmail.com>
2024-07-02 10:45:45 +0900

    유저 정보 DB 저장

---
+++ src/main/java/com/oauthjwt/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.oauthjwt.repository;
+
+import com.oauthjwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    UserEntity findByUsername(String username);
+}
